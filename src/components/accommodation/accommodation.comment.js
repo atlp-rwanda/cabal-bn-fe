065164a60071f869d1f2a63899357352030a06ec
Richard Munyemana <richmunye@gmail.com>
2022-06-06 16:54:37 +0200

    pushing

--- src/components/accommodation/accommodation.comment.js
+++ src/components/accommodation/accommodation.comment.js
@@ -46,6 +46,7 @@ const AccommodationComments = ({ accommodationState }) => {
     activeComment.id === commentId &&
     activeComment.type === 'editing';
 
+  console.log(isEditing);
   const onChange = (e) => {
     setUpdateComment(e.target.value);
   };
